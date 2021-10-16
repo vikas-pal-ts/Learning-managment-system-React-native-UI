5913879684bf879c2d9343503fde4f32c92c0d45
Vikas Pal <vikasp@capitalnumbers.com>
2021-10-16 09:01:22 +0530

    Lms by khojopaao

--- App.js
+++ App.js
@@ -1,21 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, StatusBar } from 'react-native';
+import { 
+  Rubik_300Light,
+  Rubik_300Light_Italic,
+  Rubik_400Regular,
+  Rubik_400Regular_Italic,
+  Rubik_500Medium,
+  Rubik_500Medium_Italic,
+  Rubik_700Bold,
+  Rubik_700Bold_Italic,
+  Rubik_900Black,
+  Rubik_900Black_Italic 
+} from '@expo-google-fonts/rubik';
+
+import { useFonts } from 'expo-font';
+
+import AppLoading from 'expo-app-loading';
+import Navigation from './utils/Navigation';
 
 export default function App() {
+
+  let [fontLoaded, error] = useFonts({
+    Rubik_300Light,
+    Rubik_300Light_Italic,
+    Rubik_400Regular,
+    Rubik_400Regular_Italic,
+    Rubik_500Medium,
+    Rubik_500Medium_Italic,
+    Rubik_700Bold,
+    Rubik_700Bold_Italic,
+    Rubik_900Black,
+    Rubik_900Black_Italic 
+  });
+
+  if(!fontLoaded){
+    return <AppLoading />
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Navigation />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    fontFamily: "Rubik_400Regular",
+    flex:1,
+    marginTop: StatusBar.height
   },
 });
